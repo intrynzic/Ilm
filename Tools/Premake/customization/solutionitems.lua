39255d10f46620293fbc56dd2362af9f80be3f4c
Adam Foflonker <87928802+DnA-IntRicate@users.noreply.github.com>
2026-02-02 13:36:21 +0200

    Update solutionitems.lua to work with slnx

--- Tools/Premake/customization/solutionitems.lua
+++ Tools/Premake/customization/solutionitems.lua
@@ -1,27 +1,21 @@
--- Copied from: https://github.com/TheCherno/Hazel/blob/master/vendor/premake/premake_customization/solution_items.lua
-
--- Implement the solutionitems command for solution-scope files
 require('vstudio')
 
-premake.api.register 
+-- Implement the solutionitems command for solution-scope files
+premake.api.register
 {
-	name = "solutionitems",
-	scope = "workspace",
-	kind = "list:string",
+    name = "solutionitems",
+    scope = "workspace",
+    kind = "list:string",
 }
 
-premake.override(premake.vstudio.sln2005, "projects", function(base, wks)
-	if wks.solutionitems and #wks.solutionitems > 0 then
-		local solution_folder_GUID = "{2150E333-8FDC-42A3-9474-1A3956D46DE8}" -- See https://www.codeproject.com/Reference/720512/List-of-Visual-Studio-Project-Type-GUIDs
-		premake.push("Project(\"" .. solution_folder_GUID .. "\") = \"Solution Items\", \"Solution Items\", \"{" .. os.uuid("Solution Items:" .. wks.name) .. "}\"")
-		premake.push("ProjectSection(SolutionItems) = preProject")
+premake.override(premake.vstudio.sln2026, "projects", function(base, wks)
+    if wks.solutionitems and #wks.solutionitems > 0 then
+        premake.push("<Folder Name=\"/Solution Items/\">")
+        for _, path in ipairs(wks.solutionitems) do
+            premake.w("<File Path=\"%s\" />", path)
+        end
 
-		for _, path in ipairs(wks.solutionitems) do
-			premake.w(path .. " = " .. path)
-		end
-
-		premake.pop("EndProjectSection")
-		premake.pop("EndProject")
-	end
-	base(wks)
+        premake.pop("</Folder>")
+    end
+    base(wks)
 end)
